b337b28d9d1de8fbe9da0293702c4047f12eb8e9
i.nagano78 <i.nagano78@gmail.com>
2021-08-28 08:49:05 +0900

    modified firebase(uninstalled v.9, installed v.8).

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase';
 import Header from '../components/Header';
 
 const TodoPage = (props) => {
+    const [tasks, setTasks] = useState([{id: '', title: '', body:''}]);
+    useEffect(() => {
+        const unSub = db.collection('tasks').onSnapshot((snapshot) => {
+            setTasks(
+                snapshot.docs.map((doc) => ({ id: doc.data().id, title: doc.data().title, body: doc.data().body }))
+            );
+        });
+        return () => unSub();
+    }, []);
+
     return (
         <div>
             <Header />
             <h2>Todo List</h2>
+            {tasks.map((task) =>
+                <div>
+                  <h3>{task.title}</h3>
+                    <p><span>{task.id}</span><span>{task.body}</span></p>
+                </div>
+            )}
         </div>
     )
 }
